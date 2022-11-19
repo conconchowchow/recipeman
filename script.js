48a150d47a72232bd9c951f9a79e440986a8ed2b
Connor Chow <legoman888@icloud.com>
2022-11-18 20:23:02 -0800

    update button

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
-var ingredientArray = ["egg", "tomato", "pasta"];
-var userIngredients = ["tomato", "pasta"];
-var arrayLength = ingredientArray.length;
-for (var i = 0; i < arrayLength; i++) {
-  console.log(ingredientArray[i]);
-
-
-}
+searchBtn.addEventListener('click', () => {
+    var ingredientArray = ["egg", "tomato", "pasta"];
+    var userIngredients = ["tomato", "pasta"];
+    var arrayLength = ingredientArray.length;
+    for (var i = 0; i < arrayLength; i++) {
+        console.log(ingredientArray[i]);
+    }
+});
