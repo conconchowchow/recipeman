17bb47f3fda5ebabb9bd5ea5e72c49a0fda396a7
Connor Chow <legoman888@icloud.com>
2022-11-18 20:46:06 -0800

    button

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-searchBtn.addEventListener('click', () => {
-    var ingredientArray = ["egg", "tomato", "pasta"];
-    var userIngredients = ["tomato", "pasta"];
-    var arrayLength = ingredientArray.length;
-    for (var i = 0; i < arrayLength; i++) {
-        console.log(ingredientArray[i]);
-    }
-});
+
+var ingredientArray = ["egg", "tomato", "pasta"];
+var userIngredients = ["tomato", "pasta"];
+var arrayLength = ingredientArray.length;
+for (var i = 0; i < arrayLength; i++) {
+    console.log(ingredientArray[i]);
+}
+
+function test() {
+    alert("test");
+}
