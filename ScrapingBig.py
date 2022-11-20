3be9fab837c968996e51427ca08bd1fca845e010
Waterblokey <118576768+Waterblokey@users.noreply.github.com>
2022-11-19 17:05:23 -0800

    Update ScrapingBig.py

--- ScrapingBig.py
+++ ScrapingBig.py
@@ -17,22 +17,35 @@ soup = BeautifulSoup(page.content, "html.parser")
 header_container = soup.find_all(['h1'], class_=lambda x: x != 'hidden')
 
 ingredient_container = soup.find_all("div", {'class':"comp mntl-structured-ingredients"})
+ul = soup.find('ul', class_="mntl-structured-ingredients__list")
+Others = ul.find_all("span")
 
 for lines in header_container:
     if lines.name == 'h1':
         province = lines.text
         print('Recipe: ', province, "\n")
-
+"""
 for lines in ingredient_container:
     if lines.name == 'div':
         ingredients = lines.text
         print(ingredients, "\n")
-
 """
-for lines in ingredient_container:
+"""
+new_ingredient = soup.find_all('div', {'class':"mntl-structured-ingredients"})
+for lines in new_ingredient:
     if lines.name == 'div':
-        print('')
+        stuff = lines.text
+        print(lines)
 """
+
+for lines in Others:
+#    print(lines)
+    print(lines.text)
+#    print(soup.select_one('ul.mntl-structured-ingredients__list span: last - child'))
+#    data = Others.find_all('span', data-ingredient-name="true")
+#    print(date_published)
+#    print(soup.select_one('ul.mntl-structured-ingredients__list span:last-of-type').text)
+
 # I made this function to try and connect our html file (website/frontend) and
 #our backend (python file). I do not know how it works rn.
 #def make_page():
