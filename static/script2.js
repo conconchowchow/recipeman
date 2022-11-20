9deed5813935bb950a58d669b3d7117ecc9b1b67
Connor Chow <legoman888@icloud.com>
2022-11-19 19:48:25 -0800

    Update script2.js

--- static/script2.js
+++ static/script2.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 function update() {
     // update the scene
     var text = document.getElementById("ingredients-box").value;
@@ -10,8 +12,52 @@ function update() {
     //     li.innerHTML = ingredient;
     //     ingredientsList.appendChild(li);
     // }
+    
+    // fs.readFile("recipes.txt", (err, data) => {
+    //     if (err) throw err;
+      
+    //     console.log(data.toString());
+    //   });
 
-    alert(ingredients + "\n\n" + ingredientsList);
+    const recipes = syncReadFile('./recipes.txt');
+
+    alert(recipes[0] + "\n\n" + ingredients + "\n\n" + ingredientsList);
 
 
 }
+
+// if using ES6 Imports uncomment line below
+// import {readFileSync, promises as fsPromises} from 'fs';
+const {readFileSync, promises: fsPromises} = require('fs');
+
+// read file SYNCHRONOUSLY
+function syncReadFile(filename) {
+  const contents = readFileSync(filename, 'utf-8');
+
+  const arr = contents.split(/\r?\n/);
+
+  console.log(arr); // -> ['One', 'Two', 'Three', 'Four']
+
+  return arr;
+}
+
+// syncReadFile('./example.txt');
+
+// --------------------------------------------------------------
+
+// // read file ASYNCHRONOUSLY
+// async function asyncReadFile(filename) {
+//   try {
+//     const contents = await fsPromises.readFile(filename, 'utf-8');
+
+//     const arr = contents.split(/\r?\n/);
+
+//     console.log(arr); // -> ['One', 'Two', 'Three', 'Four']
+
+//     return arr;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// asyncReadFile('./example.txt');
